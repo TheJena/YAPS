404ff92e008a16f4f80af60f797f5f4ce1546125
Marialaura Lazzaro <mar.lazzaro1@stud.uniroma3.it>
2024-07-15 16:22:25 +0100

    columns impl.

--- graph/structure.py
+++ graph/structure.py
@@ -88,6 +88,25 @@ def create_entity(
     return entity
 
 
+def create_column(value, index, instance: str = None) -> Dict[str, any]:
+    """
+    Create a provenance entity.
+    Return a dictionary with the ID and the record ID of the entity.
+
+    :param value: The value of the entity.
+    :return: A dictionary with the ID and the record ID of the entity.
+    """
+
+    column = {
+        "id": constants.NAMESPACE_COLUMN + str(uuid.uuid4()),
+        "value": value,
+        "index": index,
+        "instance": instance or [],
+    }
+
+    return column
+
+
 def create_relation(
     act_id: str,
     generated: List[any] = None,
@@ -102,6 +121,34 @@ def create_relation(
     :param generated: The list of generated entities.
     :param used: The list of used entities.
     :param invalidated: The list of invalidated entities.
+    :param same: A boolean indicating whether the generated and used entities are the same.
+    :return: None
+    """
+
+    generated = generated or []
+    used = used or []
+    invalidated = invalidated or []
+
+    if same:
+        invalidated = []
+
+    return (generated, used, invalidated, same, act_id)
+
+
+def create_relation_column(
+    act_id: str,
+    generated: List[any] = None,
+    used: List[any] = None,
+    invalidated: List[any] = None,
+    same: bool = False,
+) -> None:
+    """
+    Create a provenance relation and add it to the current relations list.
+
+    :param act_id: The ID of the activity.
+    :param generated: The list of generated columns.
+    :param used: The list of used columns.
+    :param invalidated: The list of invalidated columns.
     :param same: A boolean indicating whether the generated and used entities are the same.
     :return: None
     """
