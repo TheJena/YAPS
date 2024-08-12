ee163627ea7d8eddc234f6e00421721a9aaa5ec6
Federico Motta <federico.motta@unimore.it>
2024-08-12 14:31:37 +0200

    set GNU GPLv3+ license
    DPDS (https://github.com/Lucass97/data_provenance_for_data_science)
    was previously licensed under the MIT license (actually
    https://www.gnu.org/licenses/license-list.html#Expat); which is indeed
    compatible with GNU GPL
    
    Signed-off-by: Federico Motta <federico.motta@unimore.it>

--- pipelines/compas_pipeline.py
+++ pipelines/compas_pipeline.py
@@ -1,3 +1,29 @@
+#!/usr/bin/env python3
+# coding: utf-8
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# Copyright (C) 2024      Federico Motta            <federico.motta@unimore.it>
+#                         Pasquale Leonardo Lazzaro <pas.lazzaro@stud.uniroma3.it>
+#                         Marialaura Lazzaro        <mar.lazzaro1@stud.uniroma3.it>
+# Copyright (C) 2022-2024 Luca Gregori              <luca.gregori@uniroma3.it>
+# Copyright (C) 2021-2022 Luca Lauro                <luca.lauro@uniroma3.it>
+#
+# This file is part of a provenance capturing suite (originally named DPDS)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import sys
 
 import argparse
