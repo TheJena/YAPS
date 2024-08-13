70969396d6a8106c24072dee7f17dd72bb3b81bb
Federico Motta <federico.motta@unimore.it>
2024-08-13 12:11:37 +0200

    *.py */*.py: update *DPDS* references to YAPS
    README.md: minor improvements
    
    Signed-off-by: Federico Motta <federico.motta@unimore.it>

--- graph/logger.py
+++ graph/logger.py
@@ -9,20 +9,20 @@
 # Copyright (C) 2022-2024 Luca Gregori              <luca.gregori@uniroma3.it>
 # Copyright (C) 2021-2022 Luca Lauro                <luca.lauro@uniroma3.it>
 #
-# This file is part of a provenance capturing suite (originally named DPDS)
+# This file is part of YAPS, a provenance capturing suite
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
+# YAPS is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # any later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# YAPS is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+# License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# along with YAPS.  If not, see <https://www.gnu.org/licenses/>.
 
 import logging
 
